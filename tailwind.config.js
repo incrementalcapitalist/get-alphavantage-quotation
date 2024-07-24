366e09084816f5061dea2b7079c48f341eecfd76
incremental capital <incrementalcapital@gmail.com>
2024-07-24 20:52:06 +0900

    let tailwind know the structure.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+  "./public/index.html",
+  "./src/**/*.{js,ts,jsx,tsx}",
+  "./src/css/index.css"
+],
   theme: {
     extend: {},
   },
